6bf20beb0a1bb7cfbb4ee17af0d7bf5d4ca22fb9
georgieff1288 <georgieff1288@gmail.com>
2022-06-16 14:21:05 +0300

    Created submit form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatTableModule } from '@angular/material/table';
+
+import { EmployeesService } from "./services/employees.service";
+
 import { AppComponent } from './app.component';
 import { EmployeesComponent } from './components/employees/employees.component';
 import { EmployeesListComponent } from './components/employees-list/employees-list.component';
 import { EmployeesInfoComponent } from './components/employees-info/employees-info.component';
 import { EmployeesFormComponent } from './components/employees-form/employees-form.component';
 
-import { MatTableModule } from '@angular/material/table';
 
-import { EmployeesService } from "./services/employees.service";
+
 
 
 @NgModule({
@@ -25,6 +28,7 @@ import { EmployeesService } from "./services/employees.service";
     BrowserModule,
     AppRoutingModule,
     MatTableModule,
+    ReactiveFormsModule,
   ],
   providers: [
     EmployeesService,
--- src/app/components/employees-form/employees-form.component.ts
+++ src/app/components/employees-form/employees-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-employees-form',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employees-form.component.scss']
 })
 export class EmployeesFormComponent implements OnInit {
+  employeeForm = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    department: new FormControl('', [Validators.required]),
+    city: new FormControl('', [Validators.required]),
+    street: new FormControl('', [Validators.required])
+  });
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  get name() { return this.employeeForm.get('name'); }
+  get department() { return this.employeeForm.get('department'); }
+  get city() { return this.employeeForm.get('city'); }
+  get street() { return this.employeeForm.get('street'); }
 
+  addEmployee(){
+    console.log(this.employeeForm.value)
+  }
 }
