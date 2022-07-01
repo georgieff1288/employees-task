c8dc06617cc830648c11ab87f73ea1bb227e5dc7
georgieff1288 <georgieff1288@gmail.com>
2022-07-01 10:04:11 +0300

    Code refactoring

--- front-end/src/app/components/employees-info/employees-info.component.ts
+++ front-end/src/app/components/employees-info/employees-info.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import { EmployeesService } from "../../services/employees.service";
 import {Subscription} from "rxjs";
+import {Store} from "@ngrx/store";
+import {Employee} from "../../models/employee.model";
 
 @Component({
   selector: 'app-employees-info',
@@ -8,14 +10,15 @@ import {Subscription} from "rxjs";
   styleUrls: ['./employees-info.component.scss']
 })
 export class EmployeesInfoComponent implements OnInit, OnDestroy {
-  employeesCounter: number = 0;
+  // employeesCounter: number = 0;
+  employeesCounter = this.store.select('employees');
   subscription = new Subscription();
-  constructor(private emp: EmployeesService) { }
+  constructor(private emp: EmployeesService, private store: Store<{employees: Employee[]}>) { }
 
   ngOnInit(): void {
-    this.subscription = this.emp.numOfEmployeesObservable.subscribe(
-      res => this.employeesCounter = res
-    )
+    // this.subscription = this.emp.numOfEmployeesObservable.subscribe(
+    //   res => this.employeesCounter = res
+    // )
   }
   ngOnDestroy() {
     this.subscription.unsubscribe();
