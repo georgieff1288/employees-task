4fc33a773fdd0bb3d7dfe189573dd657f1c3589c
georgieff1288 <georgieff1288@gmail.com>
2022-07-18 09:46:20 +0300

    Chat component code refactoring

--- websocket/server.js
+++ websocket/server.js
@@ -3,8 +3,8 @@ const wss = new ws.WebSocketServer({ port: 8080 });
 
 const clients = new Set();
 wss.on('connection', function connection(ws) {
+    clients.add(ws);
     ws.on('message', function message(data) {
-        clients.add(ws);
         console.log('received: %s', data);
         for(let client of clients) {
             client.send(data);
